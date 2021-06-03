eaa50c64af2918567b368e20712471fed9a95cb5
Javad <javad.amiri@live.com>
2021-06-03 23:18:43 +0430

    In commit javad ast

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ console.log('Ali va Sajjad')
 console.log('In kare khafan')
 console.log('kare bahal')
 
+console.log('Salam be hameye React karha')
+
 // * avali 
 // |
 // * felan chizo ezafe kardam
